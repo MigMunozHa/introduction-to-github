f86686ca4a82bbdde5370ca28e8a48ba680fe66c
MigMunozHa <miguel.munoz@nottingham.ac.uk>
2023-03-30 11:38:57 +0100

    Create asd.R
    asd

---
+++ asd.R
@@ -0,0 +1,8 @@
+install.packages("tidyverse", dependencies = TRUE)
+library(tidyverse)
+install.packages("lavaan", dependencies = TRUE)
+library(lavaan)
+install.packages("usethis")
+library(usethis)
+
+use_git_config(user.name = "MigMunozHa", user.email = "miguel.munoz@nottingham.ac.uk")
